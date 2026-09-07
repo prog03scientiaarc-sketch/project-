4de13ede04651ffc516f01d2bc1c6913c705eb16
SE1 <SE1@sacho.server.com>
2026-09-07 16:35:28 +0530

    Fix media null checks and media URL routes

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
+    # Allow blank and null values so Django doesn't crash if a file goes missing
     image = models.ImageField(upload_to='items/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
