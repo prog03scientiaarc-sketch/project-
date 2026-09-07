b9d95ed4547ac2781810782d676d6b3efc7a5541
SE1 <SE1@sacho.server.com>
2026-09-07 17:12:30 +0530

    Configure Cloudinary media storage with STORAGES

--- myproject/settings.py
+++ myproject/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',  # Must be before staticfiles
     'django.contrib.staticfiles',
+    'cloudinary',  # Required
     'myapp',
 ]
 
@@ -145,13 +147,21 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Modern Django 4.2+ Storage configuration for WhiteNoise
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Automatically create media directory if it doesn't exist
