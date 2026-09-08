0dfa0c26f3d000c03be9151a6888013d20b044fa
SE1 <SE1@sacho.server.com>
2026-09-08 09:49:16 +0530

    Without Cloudinary environment

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     
-    # Serves uploaded media files in production
+    # Route for serving user media uploads directly from disk
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
