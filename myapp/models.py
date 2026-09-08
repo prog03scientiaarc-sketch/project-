b5e034ea3253b2cfdf286c10199486c89ed6d757
SE1 <SE1@sacho.server.com>
2026-09-08 10:50:10 +0530

    Downgrade Python version to 3.12 to fix Django 5 compatibility

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,10 @@
+# myapp/models.py
 from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
+    # Standard Django FileSystemStorage (no storage parameter needed)
     image = models.ImageField(upload_to='items/', blank=True, null=True)
 
     def __str__(self):
-        # Always return a plain string; avoid referencing self.image.url here
         return self.name or f"Item {self.id}"
